67ceb6f07cc1e92a56b706acf14568e98bd02b6e
Matthew Gemmell <matthewrgemmell@hotmail.com>
2022-08-19 10:44:51 +0100

    19 aug 2022

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -1,3 +1,5 @@
+import sys
+from termcolor import colored, cprint
 
-# Print "hello, world!" to the terminal
-print('Hello, World!')
+#Print hello world to the terminal
+cprint('Hello, World', 'cyan')
